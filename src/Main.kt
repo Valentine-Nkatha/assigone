6a35c7306e25574995eda406d7e71471ae051e5a
Valentine-Nkatha <valentinnenkatha@gmail.com>
2024-03-26 22:29:26 +0300

    made changes

---
+++ src/Main.kt
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    var fullName="Valentine"
+    var age=23
+    var phoneNumber="0701815843"
+    var weightInKg=72.5
+    var citizen=true
+    var notCitizen=false
+    println(fullName)
+    println(age)
+    println(phoneNumber)
+    println(weightInKg)
+    println(citizen)
+    println(notCitizen)
+}
